887d4db65a054b787eab29115d79e0fea7494360
dev34253 <dev34253@gmail.com>
2018-12-04 21:37:15 -0500

    adding difficulty and moneysupply

--- assets/js/yacoin_data.js
+++ assets/js/yacoin_data.js
@@ -5,6 +5,8 @@ class YacoinDataAccess {
         this.timestamps = [];
         this.networkhashdata = [];
         this.blocktimedata = [];
+        this.moneysupplyData = [];
+        this.difficultyData = [];
 
         this.client = stitch.Stitch.initializeDefaultAppClient('yacoinapp-iwlqx');
         this.db = this.client.getServiceClient(stitch.RemoteMongoClient.factory, 'yacoinservice').db('yacoindb');
@@ -61,9 +63,58 @@ class YacoinDataAccess {
         Plotly.react('chartTimeSinceBlock', dataTime, layoutTime, { displayModeBar: false });
     }
 
+
+    drawMoneysuply() {
+        let dataTime = [{
+            x: this.timestamps,
+            y: this.moneysupplyData
+        }]
+
+        let ts1 = this.timestamps[100];
+        let ts2 = this.timestamps[Math.round(this.timestamps.length/2)];
+        let ts3 = this.timestamps[this.timestamps.length-100];
+
+        let layoutTime = {
+            height: 400,
+            xaxis: {
+                tickvals: [ts1,ts2,ts3],
+                ticktext: [ts1,ts2,ts3]
+            },
+            yaxis: {
+                range: [0, Math.max(...this.moneysupplyData)]
+            }
+        };
+        Plotly.react('chartMoneysupply', dataTime, layoutTime, { displayModeBar: false });
+    }
+
+    drawDifficulty() {
+        let dataTime = [{
+            x: this.timestamps,
+            y: this.difficultyData
+        }]
+
+        let ts1 = this.timestamps[100];
+        let ts2 = this.timestamps[Math.round(this.timestamps.length/2)];
+        let ts3 = this.timestamps[this.timestamps.length-100];
+
+        let layoutTime = {
+            height: 400,
+            xaxis: {
+                tickvals: [ts1,ts2,ts3],
+                ticktext: [ts1,ts2,ts3]
+            },
+            yaxis: {
+                range: [0, 0.001]
+            }
+        };
+        Plotly.react('chartDifficulty', dataTime, layoutTime, { displayModeBar: false });
+    }
+
     draw() {
         this.drawHash();
         this.drawTime();
+        this.drawMoneysuply();
+        this.drawDifficulty();
     }
 
     refresh() {
@@ -91,6 +142,16 @@ class YacoinDataAccess {
                         this.timestamps.push(new Date(doc.time).toLocaleString());
                         this.networkhashdata.push(doc.networkHashPower);
                         this.blocktimedata.push(doc.timeSinceLastBlock / 60);
+                        if(doc.moneysupply){
+                            this.moneysupplyData.push(doc.moneysupply);
+                        } else {
+                            this.moneysupplyData.push(0);
+                        }
+                        if(doc.difficulty){
+                            this.difficultyData.push(doc.difficulty);
+                        } else {
+                            this.difficultyData.push(0);
+                        }
                     }
                     resolve();
                 })
